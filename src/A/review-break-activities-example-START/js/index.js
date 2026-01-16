a9d431b6f9c0bef39b0cd7ae940af89e9a702bd2
Dan Gilleland <dgilleland@nait.ca>
2026-01-16 11:06:18 -0700

    Review of JavaScript

--- src/A/review-break-activities-example-START/js/index.js
+++ src/A/review-break-activities-example-START/js/index.js
@@ -6,3 +6,25 @@ HTML for list topic list item
     NEW TOPIC HERE
 </li>
 */
+document
+    .querySelector('form')
+    .addEventListener('submit', (ev) => {
+        ev.preventDefault();  // the form does NOT submit to the backend
+        const theForm = ev.target; // <form>
+        const formControls = theForm.elements;
+        const topicInput = formControls['new-topic']; // <input type="text" name="new-topic"
+        // recall we need to use index \___________/ because the name has a hypen
+        const topic = topicInput.value; // .value gives us the content of the input control
+        console.log(topic);
+
+        if(topic.trim() === '') {
+            // Bad input 😢
+            topicInput.classList.add('is-invalid'); // this CSS class is from Bootstrap
+        } else {
+            // Good input
+            const list = document.querySelector('ul'); // there is only one <ul> on the page
+            list.innerHTML += `<li>${topic.trim()}</li>`;
+            topicInput.value = ''; // clear the input
+            topicInput.classList.remove('is-invalid');
+        }
+    });
