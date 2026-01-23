0effe8c4f19159082ab8091059c91cefd6877d06
Dan Gilleland <dgilleland@nait.ca>
2026-01-23 11:46:50 -0700

    Continue Weather App demo with the forecast rendering

---
+++ src/A/weather-app/js/dom/forecast.js
@@ -0,0 +1,43 @@
+// ~/js/dom/forecast.js
+
+/**
+ * Displays a weather forecast for a given location.
+ * 
+ * @param {Object[]} data - The array of forecast weather objects.
+ * @param {HTMLElement} el - The reference to the display DOM element.
+ */
+export const displayForecast = (data, el) => {
+    console.log(data);
+
+    let list = document.createElement('ul');
+    let currDate, nextDate, currItem;
+
+    data.forEach((item) => {
+        // destructuring for display variables
+        let {dt_txt: date} = item;
+        let {temp_max: high, temp_min: low} = item.main;
+        let {main: cond} = item.weather[0];
+
+        nextDate = date.split(' ')[0];
+        if(currDate !== nextDate) {
+            currDate = nextDate;
+
+            // create a new item
+            currItem = document.createElement('li');
+            currItem.classList.add('forecast-item');
+            currItem.innerHTML = `<p>${currDate}</p>`;
+        }
+
+        currItem.innerHTML += `
+        <div class="forecast-detail">
+            <p><strong><span class="time">Time: ${ date.substr(date.indexOf(' ')) }</span></strong></p>
+            <ul>
+                <li class="condition">Condition: ${cond}</li>
+                <li class="temp">Temp: ${ ((high + low) / 2) }</li>
+            </ul>
+        </div>`;
+
+        list.append(currItem);
+    });
+    el.append(list);
+}
