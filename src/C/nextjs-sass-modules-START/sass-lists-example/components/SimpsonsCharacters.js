b0dac9f517f4100a93191dbc9b89b24f4fe6ae7a
Dan Gilleland <dgilleland@nait.ca>
2026-02-02 09:30:22 -0700

    Demo SASS in NextJS/React App

--- src/C/nextjs-sass-modules-START/sass-lists-example/components/SimpsonsCharacters.js
+++ src/C/nextjs-sass-modules-START/sass-lists-example/components/SimpsonsCharacters.js
@@ -1,3 +1,5 @@
+import styles from '../styles/SimpsonsCharacters.module.scss';
+
 const SIMPSON_CHARACTERS = [
   "Homer Simpson",
   "Bart Simpson",
@@ -11,9 +13,12 @@ const SIMPSON_CHARACTERS = [
 ]
 
 export default function SimpsonsCharacters() {
-  return <ul>
+  return <ul className={styles.list}>
     {SIMPSON_CHARACTERS.map((characterName, index)=> {
-      return <li key={index}>{characterName}</li>
+      return <li
+        key={index}
+        className={`${styles['list-item']} ${styles.spacing}`}
+        >{characterName}</li>
     })}
   </ul>
 }
--- src/C/nextjs-sass-modules-START/sass-lists-example/next.config.js
+++ src/C/nextjs-sass-modules-START/sass-lists-example/next.config.js
@@ -1,6 +1,15 @@
+const path = require('path'); // This is the Common JS way...
+// The 'path' module is one that come with NodeJS
+// It contains utilities to safely assemble file paths
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  sassOptions: {
+    // The __dirname is a global variable (thanks to NodeJS)
+    // that holds the absolute path of our Node project's directory
+    includePaths: [path.join(__dirname, 'styles')],
+  }
 }
 
 module.exports = nextConfig
