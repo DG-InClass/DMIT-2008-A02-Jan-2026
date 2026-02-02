bf4418d2a5f884045e2b35a39288cad29cf21464
Dan Gilleland <dgilleland@nait.ca>
2026-02-02 09:06:00 -0700

    Demo MUI Theming

---
+++ src/C/nextjs-mui-theming-START/mui-theme-example/utils/theme/config.js
@@ -0,0 +1,29 @@
+// ~/utils/theme/config.js
+// Check out this site's MUI Theme Creator
+// https://bareynol.github.io/mui-theme-creator/
+import { createTheme } from '@mui/material';
+
+const themeOptions = {
+    palette: {
+        primary: {
+            light: '#757ce8',
+            main: '#3f50b5',
+            dark: '#002884',
+            contrastText: '#fff',
+        },
+        secondary: {
+            light: '#ff7961',
+            main: '#f44336',
+            dark: '#ba000d',
+            contrastText: '#000',
+        },
+    },
+    typography: {
+        h2: {
+            fontFamily: 'Lato',
+        }
+    }
+}
+
+const theme = createTheme(themeOptions);
+export { theme }
