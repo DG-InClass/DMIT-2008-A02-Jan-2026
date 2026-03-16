64eb5832a473569b6a91955ed91df6231a98bbaa
Dan Gilleland <dgilleland@nait.ca>
2026-03-16 09:41:33 -0600

    Completed Topic H

--- src/H/nextjs-pages-and-space-START/space-pages/pages/agency/[id].js
+++ src/H/nextjs-pages-and-space-START/space-pages/pages/agency/[id].js
@@ -8,24 +8,93 @@ import NavBar from '@components/NavBar';
 import SimpleDetailsCard from '@components/SimpleDetailsCard';
 
 import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+import { getAgency } from '@utils/api/agencies';
+import LoadingCircle from '@components/LoadingCircle';
 
 export default function Agency() {
+    // 1) Set up the state for this component
+    const [agencyDetails, setAgencyDetails] = useState();
+
+    // 2) Use the NextJS Router hook
     const router = useRouter();
     const { id } = router.query; // Note that my variable name matches the slug name in my component
 
+    // 3) Hooking into the mount stage of the component lifecycle
+    useEffect(() => {
+        getAgency(id).then(data => {
+            setAgencyDetails(data);
+        });
+    }, [id]); // An array of all the state info we depend upon
+
     return <>
         <NavBar />
-        <Container sx={{paddingTop:2}}>
-            <Grid container>
-                <Grid item xs="2">
-
-                </Grid>
-                <Grid item xs="10">
-                    <Typography variant="h3" gutterBottom>
-                        Agency Page for {id}.
-                    </Typography>
-                </Grid>
-            </Grid>
-        </Container>
+        {
+            !agencyDetails ?
+                <LoadingCircle />
+                :
+                <Container sx={{ paddingTop: 2 }}>
+                    <Grid container>
+                        <Grid item xs="2">
+                            <img
+                                alt={agencyDetails.name}
+                                src={agencyDetails.logo_url}
+                                style={{ width: "120px" }}
+                            />
+                        </Grid>
+                        <Grid item xs="10">
+                            <Typography variant="h3" gutterBottom>
+                                {agencyDetails.name}
+                                {agencyDetails.abbrev}
+                            </Typography>
+                        </Grid>
+                        <Grid item xs="4">
+                            <Typography variant="h5">
+                                Launch Details
+                            </Typography>
+                            <SimpleDetailsCard
+                                title="Total Launches"
+                                description={agencyDetails.total_launch_count}
+                            />
+                            <SimpleDetailsCard
+                                title="Successful Launches"
+                                description={agencyDetails.successful_launches}
+                            />
+                            <SimpleDetailsCard
+                                title="Successful Landings"
+                                description={agencyDetails.successful_landings}
+                            />
+                        </Grid>
+                        <Grid item xs="4">
+                            <Typography variant="h5">Agency Information</Typography>
+                            <SimpleDetailsCard
+                                title="Administrator"
+                                description={agencyDetails.administrator}
+                            />
+                            <SimpleDetailsCard
+                                title="Sapce Agency Details"
+                                description={`Founded ${agencyDetails.founding_year}`}
+                                subDescription={agencyDetails.description}
+                            />
+                        </Grid>
+                        <Grid item xs="4">
+                            <Typography variant="h5">
+                                {`SpaceCraft`}
+                            </Typography>
+                            {agencyDetails.spacecraft_list && agencyDetails.spacecraft_list.map((spaceCraft) => {
+                                return <SimpleDetailsCard
+                                    key={spaceCraft.id}
+                                    description={`${spaceCraft.name}`}
+                                    buttonCallback={() => {
+                                        // TODO: Have the router link/go to the page with the spacecraft details.
+                                        console.log("go to the space page")
+                                    }}
+                                    buttonName="Go to SpaceCraft"
+                                />
+                            })}
+                        </Grid>
+                    </Grid>
+                </Container>
+        }
     </>
 }
---
+++ src/H/nextjs-pages-and-space-START/space-pages/pages/spacecraft/[spaceCraftId].js
@@ -0,0 +1,50 @@
+// pages/spacecraft/[spaceCraftId].js
+
+import { Typography, Container, Grid } from '@mui/material';
+import NavBar from '@components/NavBar';
+import SimpleDetailsCard from '@components/SimpleDetailsCard';
+import LoadingCircle from '@components/LoadingCircle';
+
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+import { getSpaceCraft } from '@utils/api/spaceCraft';
+
+export default function SpaceCraft() {
+    const [spaceCraftDetails, setSpaceCraftDetails] = useState();
+    const router = useRouter();
+    const { spaceCraftId } = router.query;
+
+    useEffect(() => {
+        getSpaceCraft(spaceCraftId).then(data => {
+            setSpaceCraftDetails(data);
+        });
+    }, [spaceCraftId]);
+
+    return <>
+        <NavBar />
+        {
+            !spaceCraftDetails ? <LoadingCircle /> :
+            <Container sx={{paddingTop:2}}>
+                <Grid container>
+                    <Grid xs="12" item>
+                        <Typography variant="h3" gutterBottom>
+                            {spaceCraftDetails.name}
+                        </Typography>
+                    </Grid>
+                    <Grid xs="4">
+                        <SimpleDetailsCard
+                            title={`${spaceCraftDetails.name} details`}
+                            subDescription={spaceCraftDetails.description}
+                        />
+                    </Grid>
+                    <Grid xs="4">
+                        <SimpleDetailsCard
+                            title={`${spaceCraftDetails.name} configuration`}
+                            subDescription={spaceCraftDetails.spacecraft_config.details}
+                        />
+                    </Grid>
+                </Grid>
+            </Container>
+        }
+    </>
+}
