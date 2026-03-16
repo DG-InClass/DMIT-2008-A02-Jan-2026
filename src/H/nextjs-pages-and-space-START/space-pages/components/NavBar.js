4f9222f89922e865f1506bf758e3697c8481759e
Dan Gilleland <dgilleland@nait.ca>
2026-03-16 08:23:50 -0600

    Navigation and basic data on space agencies

--- src/H/nextjs-pages-and-space-START/space-pages/components/NavBar.js
+++ src/H/nextjs-pages-and-space-START/space-pages/components/NavBar.js
@@ -1,17 +1,26 @@
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography'
+import Typography from '@mui/material/Typography';
+
+import Link from 'next/link';
 
 
 export default function NavBar(props) {
+  const home = "/";
+  const about = "/about/";
   return <AppBar position="static">
     <Toolbar>
-      <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-          Space Agency App
-      </Typography>
-      <Typography variant="h6" component="div" >
-        About
-      </Typography>
+      <Link href={home}>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Space Agency App
+        </Typography>
+      </Link>
+
+      <Link href={about}>
+        <Typography variant="h6" component="div" >
+          About
+        </Typography>
+      </Link>
     </Toolbar>
   </AppBar>
 }
--- src/H/nextjs-pages-and-space-START/space-pages/pages/index.js
+++ src/H/nextjs-pages-and-space-START/space-pages/pages/index.js
@@ -7,8 +7,18 @@ import Box from '@mui/material/Box';
 import AgencyCard from '@components/AgencyCard';
 import NavBar from '@components/NavBar';
 
+import { useEffect, useState } from 'react';
+import { getAgencies } from '@utils/api/agencies';
 
 export default function Home() {
+  const [agenciesData, setAgenciesData] = useState([]);
+
+  useEffect(() => {
+    getAgencies().then(data => {
+      console.log(data);
+      setAgenciesData(data.results);
+    })
+  }, []);
 
   return (
     <div>
@@ -33,6 +43,16 @@ export default function Home() {
               alignItems: 'center',
             }}
           >
+            {agenciesData.map(agency => 
+              <AgencyCard
+                key={agency.id}
+                id={agency.id}
+                imageUrl={agency.image_url}
+                name={agency.name}
+                abbreviation={agency.abbrev}
+                description={agency.description} 
+              />
+            )}
           </Box>
         </Container>
 
