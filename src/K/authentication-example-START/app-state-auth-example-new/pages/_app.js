4d5d9f45bb7e3e018dac69d600be4170e688c2f0
Dan Gilleland <dgilleland@nait.ca>
2026-04-13 08:38:24 -0600

    Use the AppProviders

--- src/K/authentication-example-START/app-state-auth-example-new/pages/_app.js
+++ src/K/authentication-example-START/app-state-auth-example-new/pages/_app.js
@@ -1,6 +1,8 @@
 import '@/styles/globals.css'
+import AppProviders from '@/components/state/AppProviders';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
-
+  return <AppProviders>
+      <Component {...pageProps} />
+    </AppProviders>
 }
